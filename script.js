25135c02afe97340e45eb9b2ebffea5546982ce3
Rolando2 <rolando.far9@gmail.com>
2024-08-29 18:06:02 -0500

    animation js 1.0

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function unTouch() {
 
 }
 
+/*
 function seeCer(){
 
   const backgroundImg = document.getElementById('ImgBox');
@@ -37,6 +38,7 @@ function seeCer(){
   coupleImg.style.scale = 0.96;
 
   LT1.style.animation = "vanish 0.8s 0.1s forwards";
+  LT1.style.animation = "vanish 0.8s 0.1s forwards";
   LT2.style.animation = "vanish 0.8s 0.2s forwards";
   LT3.style.animation = "vanish 0.8s 0.4s forwards";
   BB1.style.animation = "vanishGrid 0.8s 0.4s forwards";
@@ -44,10 +46,47 @@ function seeCer(){
 
   const directionCer = document.getElementById('dirCer');
 
-  directionCer.style.animation = "appearGrid 1s 1s forwards";
+  directionCer.classList.toggle("appear");
 
-  
+  const ltI1 = document.getElementById('ltI1');
+  const ltC1 = document.getElementById('ltC1');
+  const ltC2 = document.getElementById('ltC2');
+  const ltH = document.getElementById('ltH');
+  const ltI2 = document.getElementById('ltI2');
+  const ltC3 = document.getElementById('ltC3');
+  const ltDB = document.getElementById('ltDB');
+  const ltRB = document.getElementById('ltRB');
+
+  ltI1.style.animation = "appear 0.5s 2s forwards";
 }
+*/
+
+const rtnBtn = document.getElementById('ltRB');
+
+function returnToStart(){
+  const dirCer = document.getElementById('dirCer');
+  const dirPar = document.getElementById('dirPar');
+  
+  if (dirCer){
+    console.log("Correct");
+    var disappear = document.querySelectorAll(".disapear");
+
+    for(var i=0; i<disappear.length; i++){
+      (function (i) {
+        setTimeout(function () {disappear[i].style.animation = "vanishGrid 0.8s forwards"},i*15)
+    }(i))
+    }
+
+  }else if (dirPar) {
+    console.log("Incorrect");
+  }
+
+}
+
+rtnBtn.addEventListener('click', returnToStart);
+
+
+
 
 // Set the date we're counting down to
 var countDownDate = new Date("Oct 12, 2024 12:00:00").getTime();
